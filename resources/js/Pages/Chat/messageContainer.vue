21c8ece8078388242aec3a62cbc81473172ed15b
Reza Dastras <ehsan1372d@gmail.com>
2021-07-11 09:07:46 -0700

    upload all changes

---
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="h-96 w-full">
+        <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+            <div v-for="(message, index) in messages" :key="index">
+                 <detail-message :message="message" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import detailMessage from './detailMessage.vue'
+
+export default {
+  components: { detailMessage },
+    
+    props: ['messages'],
+  
+}
+</script>
